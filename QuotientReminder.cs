195daaa84a7211bb1da49b3721b5c7281269cb73
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-08-31 15:59:30 +0530

    added QuotientReminder.cs

---
+++ QuotientReminder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_core_program
+{
+    public class QuotientReminder
+    {
+        public static void CalculateQuotietAndRemainder()
+        {
+            int dividend = 100;
+            int devisor = 25;
+            int quotient = dividend / devisor;
+            int reminder = dividend % devisor;
+            Console.WriteLine("Dividend:" + dividend + "Divisor:" + devisor);
+            Console.WriteLine("Quotient:" + quotient);
+            Console.WriteLine("Reminder:" + reminder);
+        }
+    }
+}
