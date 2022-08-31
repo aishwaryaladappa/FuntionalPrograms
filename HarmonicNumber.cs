6a9f8c4f438b8cb466d1863f15c008067cc43e2c
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-08-31 13:43:51 +0530

    added HarmonicNumber.cs

---
+++ HarmonicNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_core_program
+{
+    public class HarmonicNumber
+    {
+        public static void DisplayHarmonicNumber()
+        {
+            int i, n, Value = 0;
+            Console.WriteLine("enter the number");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+
+                Console.Write("(1 /{0}) +", i);
+
+            }
+        }
+    }
+}
