bb831aca256529c2f32d99c39ada3322e076811e
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-08-31 13:41:15 +0530

    added EvenOdd.cs

---
+++ EvenOdd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_core_program
+{
+    public static class EvenOdd
+    {
+        public static void CheckEvenOdd()
+        {
+            int number;
+            Console.WriteLine("enter a number:");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("number is even number");
+
+            }
+            else
+            {
+                Console.WriteLine("number is odd number");
+            }
+
+
+        }
+
+    }
+}
