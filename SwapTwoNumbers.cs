6a064c63e7a23f8ef75b73be472d67a2085ed9d2
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-08-31 16:00:39 +0530

    added SwapTwoNumbers.cs

---
+++ SwapTwoNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_core_program
+{
+    public class SwapTwoNumbers
+    {
+        public static void Swapped()
+        {
+            int a = 10, b = 7;
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("a:" + a + "b:" + b);
+
+        }
+    }
+}
