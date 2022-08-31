a87d5101fc4078a5e6cf45ba8f257146a41d491c
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-08-31 14:59:51 +0530

    added PowerOfTwo.cs

---
+++ PowerOfTwo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_core_program
+{
+    public class PowerOfTwo
+    {
+        public static void CalculatePwowerOfTwo()
+        {
+            int limit, number, i, mul;
+            Console.WriteLine("enter the number");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the limit");
+            limit = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i <= limit; i++)
+            {
+                mul = number * i;
+                Console.WriteLine("power of two is" + mul);
+            }
+
+        }
+
+
+    }
+}
